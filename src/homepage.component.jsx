99c5116b06d489a09315c3b3726006bb2590a2af
CaitlynFowkes <caitlyn@gradconnection.com>
2020-03-29 15:48:05 +0200

    Set up initial project and add scss

---
+++ src/homepage.component.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">BAGS</h1>
+            <span className="sub-title">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">JACKETS</h1>
+            <span className="sub-title">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">SHOES</h1>
+            <span className="sub-title">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">TOPS</h1>
+            <span className="sub-title">SHOP NOW</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">PANTS</h1>
+            <span className="sub-title">SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+    </div> 
+  );
+};
