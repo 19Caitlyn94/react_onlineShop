65d65f23de97bcedc860223c7e001eec67031f5e
CaitlynFowkes <caitlynfowkes@gmail.com>
2020-04-04 18:33:04 +0200

    Add routes for categories

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,16 +1,23 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size}) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
   return (
-    <div className={`menu-item ${size}`}>
-    <div style={{ backgroundImage: `url(${imageUrl})` }} className="background-image" />
+    <div
+      className={`menu-item ${size}`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+      <div
+        style={{ backgroundImage: `url(${imageUrl})` }}
+        className="background-image"
+      />
       <div className="content">
         <h1 className="title">{title.toUpperCase()}</h1>
         <span className="sub-title">SHOP NOW</span>
       </div>
     </div>
   );
-}
+};
 
-export default MenuItem;
+export default withRouter(MenuItem);
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./homepage.styles.scss";
 import Directory from "../../components/directory/directory.component";
 
-export const Homepage = () => {
+export const HomePage = () => {
   return (
     <div className="homepage">
       <Directory />
