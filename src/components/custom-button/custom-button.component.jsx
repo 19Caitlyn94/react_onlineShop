886f227b8210531e5651a07f9047dd24da1571c2
CaitlynFowkes <caitlynfowkes@gmail.com>
2020-05-31 20:13:52 +0200

    Style add to cart button in collection item

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import "./custom-button.styles.scss";
 
-export default function Button({ children, isGoogleSignIn, ...otherProps }) {
+export default function Button({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) {
   return (
     <button
-      className={`${isGoogleSignIn ? "google-sign-in" : ""} custom-button`}
+      className={`${inverted ? "inverted" : ""} ${
+        isGoogleSignIn ? "google-sign-in" : ""
+      } custom-button`}
       {...otherProps}
     >
       {children}
