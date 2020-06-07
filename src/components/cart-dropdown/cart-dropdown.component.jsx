0f4708dfba14d75b14e44d9dc8d692769da1dde2
CaitlynFowkes <caitlynfowkes@gmail.com>
2020-06-07 22:28:04 +0200

    Add the cartitems selector

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Button from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
+
+import { selectCartItems } from '../../redux/cart/cart.selectors';
+
 import './cart-dropdown.styles.scss';
 
-export default function Cart() {
+function Cart({ cartItems }) {
   return (
     <div className='cart-dropdown'>
+      {cartItems.map(item => {
+        return <CartItem key={item.id} item={item} />;
+      })}
       <div className='cart-items'></div>
       <Button>GO TO CHECKOUT</Button>
     </div>
   );
 }
+
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state),
+});
+
+export default connect(mapStateToProps)(Cart);
